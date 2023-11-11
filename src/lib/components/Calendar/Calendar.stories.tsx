afc640ef33c1fb2874f89a666fc0103ef8b3a0f5
nlom0218 <nlom0218@naver.com>
2023-11-11 22:20:05 +0900

    test: ClickDayCalendar 스토리 추가

--- src/lib/components/Calendar/Calendar.stories.tsx
+++ src/lib/components/Calendar/Calendar.stories.tsx
@@ -2,6 +2,8 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import Calendar from './Calendar';
 
+import format from '../../utils/format';
+
 type Story = StoryObj<typeof Calendar>;
 
 /**
@@ -34,5 +36,17 @@ export const HandleCalendar: Story = {
     month: new Date().getMonth() + 1,
     onChangeCalendar: (year, month) =>
       alert(`${year}년, ${month}월로 이동합니다!`),
+  },
+};
+
+/**
+ * ClickDayCalendar는 달력의 day(일)을 클릭하면 특정 함수가 호출되는 스토리입니다.
+ * 화살표를 클릭해보세요.
+ */
+export const ClickDayCalendar: Story = {
+  args: {
+    year: new Date().getUTCFullYear(),
+    month: new Date().getMonth() + 1,
+    onClickDay: (date) => alert(`${format.date(date)}을 클릭했어요.`),
   },
 };
